a43cfcd76be914d552cb6ef56008e3693391d24c
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-03-14 23:21:43 +0100

    feat: Initialize form on page 1 (plan)

--- pages/select-plan.tsx
+++ pages/select-plan.tsx
@@ -1,5 +1,48 @@
-import React from "react";
+import { useAppContext } from "@/context/app-context";
 
 export default function SelectPlan() {
-  return <div>select-plan</div>;
+  const { plan, planDuration, handleChange } = useAppContext();
+  return (
+    <div>
+      <h2>Select your plan</h2>
+      <p>You have the option of monthly or yearly billing.</p>
+      <label htmlFor="arcade">Arcade $9/mo</label>
+      <input
+        type="radio"
+        name="plan"
+        value="arcade"
+        id="arcade"
+        checked={plan === "arcade"}
+        onChange={handleChange}
+      />
+      <label htmlFor="advanced">Advanced $12/mo</label>
+      <input
+        type="radio"
+        name="plan"
+        value="advanced"
+        id="advanced"
+        checked={plan === "advanced"}
+        onChange={handleChange}
+      />
+
+      <label htmlFor="monthly">Monthly</label>
+      <input
+        type="radio"
+        name="planDuration"
+        value="monthly"
+        id="monthly"
+        checked={planDuration === "monthly"}
+        onChange={handleChange}
+      />
+      <label htmlFor="yearly">Yearly</label>
+      <input
+        type="radio"
+        name="planDuration"
+        value="yearly"
+        id="yearly"
+        checked={planDuration === "yearly"}
+        onChange={handleChange}
+      />
+    </div>
+  );
 }
