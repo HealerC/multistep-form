e04415bfee50adebf84011c3d747e6d09607c5dc
Uyioghosa Nosa-Ihaza <ihazauyi@gmail.com>
2023-03-15 22:15:09 +0100

    feat: For now redirect from homepage to step 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,12 @@
+import React from "react";
 import Layout from "@/components/Layout";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  React.useEffect(() => {
+    router.replace("/your-info");
+  });
   return (
     <Layout>
       <h1>Hello world</h1>
